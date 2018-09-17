fa11933e2b8c63f89e06b0e8cdebf0c6c36d66fa
StphnLwnga <boywilder99@gmail.com>
2018-09-17 19:28:12 +0300

    nakalaafrica

--- routes/index.js
+++ routes/index.js
@@ -5,12 +5,14 @@ const adminController = require('../controllers/adminController');
 const itemController = require('../controllers/itemController');
 const { catchErrors } = require('../handlers/errorHandlers');
 
-/* Administrator dashboard */
+/* Login */
 router.get('/login', adminController.loginForm);
 router.post('/login', authController.login);
 
 router.get('/logout', authController.logout);
 
+/* ADMIN DASHBOARD */
+/* Dash Home*/
 router.get(
 	'/admindash',
 	authController.isLoggedIn,
@@ -19,18 +21,56 @@ router.get(
 router.get(
 	'/admindash/inventory',
 	authController.isLoggedIn,
-	catchErrors(adminController.getStore),
+	catchErrors(itemController.getStore),
 );
+
+/* Edit Inventory*/
 router.post(
 	'/admindash/additem',
 	authController.isLoggedIn,
-	catchErrors(adminController.addItem),
+	catchErrors(itemController.addItem),
+);
+router.post(
+	'/admindash/additem/:id',
+	authController.isLoggedIn,
+	catchErrors(itemController.addItem),
 );
 router.get(
-	'/admindash/edititem',
+	'/admindash/:id/edit',
 	authController.isLoggedIn,
-	catchErrors(adminController.editItem),
+	catchErrors(itemController.editItem),
+);
+router.get(
+	'/admindash/:id/edit',
+	authController.isLoggedIn,
+	catchErrors(itemController.deleteItem),
 );
 
+/* Edit Admins */
+router.post(
+	'/admindash/addadmin',
+	authController.isLoggedIn,
+	catchErrors(adminController.addAdmin),
+);
+router.post(
+	'/admindash/addadmin/:id',
+	authController.isLoggedIn,
+	catchErrors(adminController.addAdmin),
+);
+router.post(
+	'/admindash/editadmin/:id',
+	authController.isLoggedIn,
+	catchErrors(adminController.editAdmin),
+);
+router.post(
+	'/admindash/editadmin/:id',
+	authController.isLoggedIn,
+	catchErrors(adminController.deleteAdmin),
+);
+router.get(
+	'/admindash/admins',
+	authController.isLoggedIn,
+	catchErrors(adminController.getAdmins)
+);
 
 module.exports = router;
